80601ae581f3f685d0ad410e472c16ad874400bf
Judzhin Miles <info@msbios.com>
2016-12-07 17:58:34 +0200

    Intermediate commit

---
+++ src/MSBios/ModelBundle/Form/PostType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace MSBios\ModelBundle\Form;
+
+use MSBios\ModelBundle\Entity\Author;
+use MSBios\ModelBundle\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PostType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('slug')
+            ->add('description')
+            ->add('author')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => Post::class
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'msbios_modelbundle_post';
+    }
+}
